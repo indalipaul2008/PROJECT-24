f780336b83cfb9c7b940b44cd6ac06bc81273e52
indalipaul2008 <75675799+indalipaul2008@users.noreply.github.com>
2021-01-11 13:24:06 +0530

    Update ground.js

--- ground.js
+++ ground.js
@@ -6,7 +6,7 @@ class Ground{
         friction:0,
         density:1, 
     }
-    this.body = bodies.rectangle(x,y,width,height,options)
+    this.body = Bodies.rectangle(x,y,width,height,options)
     this.width = width
     this.height = height
 
@@ -16,6 +16,6 @@ class Ground{
 display(){
 rectMode(CENTER)
 fill("yellow")
-(this.body.position.x,this.body.position.y,this.width,this.height)
+rect(this.body.position.x,this.body.position.y,this.width,this.height)
 }
 }
