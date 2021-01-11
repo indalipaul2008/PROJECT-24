ad985fd32a0e4934be16982e94b72a4afb9d167e
indalipaul2008 <75675799+indalipaul2008@users.noreply.github.com>
2021-01-11 13:26:10 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,8 +4,8 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-var paper,
-var ground,
+//var paper,
+//var ground,
 var side1,side2,side3
 
 function setup() {
@@ -19,9 +19,9 @@ function setup() {
 	//Create the Bodies Here.
 	paper = new Paper(100,600,10)
 	ground = new Ground(400,680,800,20)
-	side1 = new Dustbin(550,620,20,100)
-	side2 = new Dustbin(550,620,150,20)
-	side3 = new Dustbin(550,620,20,100)
+	side1 = new Dustbin(750,610,20,70)
+	side2 = new Dustbin(600,610,20,70)
+	side3 = new Dustbin(675,655,170,20)
 
 
 
@@ -46,7 +46,7 @@ function draw() {
 
 function keyPressed(){
 	if(keyCode===UP_ARROW){
-		Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85})
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:15,y:-15})
 	}
 }
 
