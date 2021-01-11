d05f4f59c6c4f084a3e628bc2032515b29ddcffe
indalipaul2008 <75675799+indalipaul2008@users.noreply.github.com>
2021-01-11 07:57:34 +0530

    Update ground.js

--- ground.js
+++ ground.js
@@ -16,6 +16,6 @@ class Ground{
 display(){
 rectMode(CENTER)
 fill("yellow")
-circle(this.body.position.x,this.body.position.y,this.width,this.height)
+(this.body.position.x,this.body.position.y,this.width,this.height)
 }
 }
